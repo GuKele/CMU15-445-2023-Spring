ee282672c013d866601f4bbea49a27eedcb83428
IAmFn <805926222@qq.com>
2023-06-23 19:29:08 +0800

    temp save update implement chage index change

--- src/execution/update_executor.cpp
+++ src/execution/update_executor.cpp
@@ -9,8 +9,10 @@
 // Copyright (c) 2015-2021, Carnegie Mellon University Database Group
 //
 //===----------------------------------------------------------------------===//
+#include <cstddef>
 #include <cstdint>
 #include <memory>
+#include <unordered_set>
 #include <vector>
 
 #include "common/config.h"
@@ -92,8 +94,15 @@ auto UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
   while (child_executor_->Next(&old_tuple, &old_rid)) {
     std::vector<Value> values{};
     values.reserve(child_executor_->GetOutputSchema().GetColumnCount());
-    for (const auto &expr : plan_->target_expressions_) {
-      values.emplace_back(expr->Evaluate(&old_tuple, child_executor_->GetOutputSchema()););
+    std::unordered_set<uint32_t> diff_key_attrs;
+    for (std::size_t i = 0; i < plan_->target_expressions_.size() ; ++i) {
+      const auto &expr = plan_->target_expressions_[i];
+      auto new_value = expr->Evaluate(&old_tuple, child_executor_->GetOutputSchema());
+      auto old_value = old_tuple.GetValue(&child_executor_->GetOutputSchema(), i);
+      if(new_value.CompareEquals(old_value) == CmpBool::CmpFalse || new_value.CompareEquals(old_value) == CmpBool::CmpNull) {
+        diff_key_attrs.insert(i);
+      }
+      values.emplace_back(new_value);
     }
     auto new_tuple = Tuple{std::move(values), &child_executor_->GetOutputSchema()};
 
@@ -103,9 +112,22 @@ auto UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
     // table_info->table_->UpdateTupleMeta(TupleMeta{INVALID_TXN_ID, INVALID_TXN_ID, true}, old_rid);
     // TODO(gukele): 只更新改变了的索引
     for (auto index_info : indexs_info) {
-      auto old_key_tuple = old_tuple.KeyFromTuple(child_executor_->GetOutputSchema(), index_info->key_schema_,
-                                                  index_info->index_->GetKeyAttrs());
-      index_info->index_->DeleteEntry(old_key_tuple, old_rid, exec_ctx_->GetTransaction());
+      bool change = false;
+      for(auto key_attr : index_info->index_->GetKeyAttrs()) {
+        if(diff_key_attrs.count(key_attr) == 1) {
+          change = true;
+          break;
+        }
+      }
+      if(change) {
+        auto old_key_tuple = old_tuple.KeyFromTuple(child_executor_->GetOutputSchema(), index_info->key_schema_,
+                                                    index_info->index_->GetKeyAttrs());
+        index_info->index_->DeleteEntry(old_key_tuple, old_rid, exec_ctx_->GetTransaction());
+
+        auto new_key_tuple = new_tuple.KeyFromTuple(child_executor_->GetOutputSchema(), index_info->key_schema_,
+                                                    index_info->index_->GetKeyAttrs());
+        index_info->index_->InsertEntry(new_key_tuple, old_rid, exec_ctx_->GetTransaction());
+      }
     }
     // insert new
     // TODO(gukele) why insert bug!!!
@@ -114,12 +136,13 @@ auto UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
     // TODO(gukele)
     // 不知道什么插入操作后while循环不退出，使得删除再插入来模拟更新无法实现，所以使用了update(文档中说除非为了project4冲榜，否则不要用)
     table_info->table_->UpdateTupleInPlaceUnsafe({}, new_tuple, old_rid);
-    for (auto index_info : indexs_info) {
-      auto new_key_tuple = new_tuple.KeyFromTuple(child_executor_->GetOutputSchema(), index_info->key_schema_,
-                                                  index_info->index_->GetKeyAttrs());
-      index_info->index_->InsertEntry(new_key_tuple, old_rid, exec_ctx_->GetTransaction());
-      // index_info->index_->InsertEntry(new_key_tuple, *new_rid, exec_ctx_->GetTransaction());
-    }
+
+    // for (auto index_info : indexs_info) {
+    //   auto new_key_tuple = new_tuple.KeyFromTuple(child_executor_->GetOutputSchema(), index_info->key_schema_,
+    //                                               index_info->index_->GetKeyAttrs());
+    //   index_info->index_->InsertEntry(new_key_tuple, old_rid, exec_ctx_->GetTransaction());
+    //   // index_info->index_->InsertEntry(new_key_tuple, *new_rid, exec_ctx_->GetTransaction());
+    // }
 
     ++update_cnt;
   }
